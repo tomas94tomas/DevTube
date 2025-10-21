2778e10730e232ce5cfcbc4d0ad65fa47cd560a4
tomas94tomas <tomas011@gmail.com>
2025-10-21 19:50:15 +0300

    Add S3 upload and presigned URL functions
    Implement S3 file upload and presigned URL generation.

--- app/s3_utils.py
+++ app/s3_utils.py
@@ -1,1 +1,30 @@
+import os
+import boto3
+from botocore.exceptions import ClientError
 
+
+BUCKET = os.getenv("AWS_S3_BUCKET", "")
+REGION = os.getenv("AWS_REGION", "eu-central-1")
+
+
+_session = boto3.session.Session(region_name=REGION)
+s3 = _session.client("s3")
+
+
+def upload_fileobj(fileobj, key, content_type):
+extra = {"ContentType": content_type}
+s3.upload_fileobj(fileobj, BUCKET, key, ExtraArgs=extra)
+return key
+
+
+
+
+def presigned_url(key, expires=3600):
+try:
+return s3.generate_presigned_url(
+"get_object",
+Params={"Bucket": BUCKET, "Key": key},
+ExpiresIn=expires,
+)
+except ClientError:
+return None
