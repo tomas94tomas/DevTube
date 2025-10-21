9480baff2593cc1a3164772742f07fb6407e57b4
tomas94tomas <tomas011@gmail.com>
2025-10-21 19:59:00 +0300

    Add variables for AWS configuration in Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,5 @@
-
+variable "aws_region" { default = "eu-central-1" }
+variable "project"    { default = "devtube" }
+variable "instance_type" { default = "t3.micro" }
+variable "key_name" { description = "EC2 key pair name" }
+variable "public_key_path" { description = "Path to your SSH public key" }
