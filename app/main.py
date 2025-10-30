57080530a403d0fdb71720f1ca99749dfcaa20d5
Tomas T. <tomas011@gmail.com>
2025-10-30 17:18:08 +0200

    fix: use package-relative imports in app.main

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from urllib.parse import urlparse, parse_qs
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
-from models import init_db, query, execute
-from s3_utils import upload_fileobj, presigned_url, delete_object
+from .models import init_db, query, execute
+from .s3_utils import upload_fileobj, presigned_url
 
 load_dotenv()
 app = Flask(__name__)
